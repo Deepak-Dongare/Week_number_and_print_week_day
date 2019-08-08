e025ce1eb1592cca3c0da80b3f4752596e66c630
Deepak Dongare <52232024+Deepak-Dongare@users.noreply.github.com>
2019-08-08 12:58:03 -0700

    Week_number_and_print_week_day

---
+++ Week_number_and_print_week_day.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Week_number_and_print_week_day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter week number");
+		int a=sc.nextInt();
+		int day=a*7;
+		System.out.println("Total day"+day);
+		
+	}
+
+}
